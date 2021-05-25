99074d3a8d229982b50af35e7a2e2f35db12bd27
Luc Saunders <luc.saunders@terazo.com>
2021-05-25 17:15:19 -0400

    Update package.json and webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,39 @@
+const DotEnvWebpack = require("dotenv-webpack")
+
 module.exports = (config, { isProd, isDev, isTest }) => {
   /**
-   * Customize the webpack by modifying the config object.
-   * Consult https://webpack.js.org/configuration for more information
-   */
 
-  return config;
+     * Customize the webpack by modifying the config object.
+
+     * Consult https://webpack.js.org/configuration for more information
+
+     */
+
+  // We dynamically change the path to the .env that contains the file corresponding to our profile
+
+  let envPath
+
+  switch (process.env.FLEX_TWILIO_PROFILE) {
+    case "FlexDev":
+      envPath = ".env.development"
+
+      break
+
+    case "Flex":
+      envPath = ".env.production"
+
+      break
+  }
+
+  // If path was set, use the dotenv-webpack to inject the variables
+
+  if (envPath) {
+    config.plugins.push(
+      new DotEnvWebpack({
+        path: envPath,
+      }),
+    )
+  }
+
+  return config
 }
